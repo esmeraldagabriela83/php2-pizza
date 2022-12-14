260c95068e5ab988a7041306c310b0566020b3c4
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2022-12-14 21:36:45 +0200

    add inputs to the contact php page , terms and conditions

--- php-form-pizza/contact.php
+++ php-form-pizza/contact.php
@@ -77,6 +77,8 @@ body { margin: 0; padding: 0; }
   <div class="container">
     <div class="container-form">
       <div class="header">
+        <h2>Portfolio site</h2>
+        <h3 style="text-align:center ; color:floralwhite">Do not fill in the form with your real data</h3>
         <h2>Create Account</h2>
       </div>
 
@@ -84,23 +86,53 @@ body { margin: 0; padding: 0; }
 
         <div class="form-control">
           <label for="username">Username</label>
-          <input type="text" placeholder="pizzaConsumer" id="username" name="name" required />
+          <input type="text" placeholder="pizzaConsumer" id="username" name="name" value="
+<?php
+//ca sa ramana bifat de catre user
+if(isset($_POST['name'])){echo $_POST['name'] ; }
+ ?>
+          " required />
 
           <i class="fa fa-check-circle"></i>
           <i class="fa fa-exclamation-circle"></i>
           <small>Error message</small>
         </div>
+
         <div class="form-control">
           <label for="username">Email</label>
-          <input type="email" placeholder="a@pizza-portfolio.com" id="email" name="email" required />
-
+          <input type="email" placeholder="a@pizza-portfolio.com" id="email" name="email" value="
+<?php
+//ca sa ramana bifat de catre user
+if(isset($_POST['email'])){echo $_POST['email'] ; }
+ ?>
+          " required />
           <i class="fa fa-check-circle"></i>
           <i class="fa fa-exclamation-circle"></i>
           <small>Error message</small>
         </div>
+
+
+
+      <div class="form-control">
+        <label for="dateBirthdayId">Your birthday:</label><br>
+            <input type="date" name="dateBirthday" id="dateBirthdayId" value=" <?php
+            if( isset($_POST['dateBirthday']) ){
+                //CA SA RAMANA BIFAT DE CATRE USER
+               echo $_POST['dateBirthday'] ;
+            }
+            ?> " />
+       </div>
+
+
+
         <div class="form-control">
           <label for="username">Password</label>
-          <input type="password" placeholder="Password" id="password" name="password" required />
+          <input type="password" placeholder="Password" id="password" name="password" value="
+          <?php
+          //ca sa ramana bifat de catre user
+          if(isset($_POST['password'])){echo $_POST['password'] ; }
+           ?>
+          " required />
 
           <i class="fa fa-check-circle"></i>
           <i class="fa fa-exclamation-circle"></i>
@@ -108,7 +140,12 @@ body { margin: 0; padding: 0; }
         </div>
         <div class="form-control">
           <label for="username">Password check</label>
-          <input type="password" placeholder="Password two" id="password2" name="password2" required />
+          <input type="password" placeholder="Password two" id="password2" name="password2" value="
+          <?php
+          //ca sa ramana bifat de catre user
+          if(isset($_POST['password2'])){echo $_POST['password2'] ; }
+           ?>
+          " required />
 
           <i class="fa fa-check-circle"></i>
           <i class="fa fa-exclamation-circle"></i>
@@ -118,10 +155,14 @@ body { margin: 0; padding: 0; }
         <!-- add comment -->
 
         <div class="form-control">
-          <label for="username">Write your comment</label>
+          <label for="comment">Write your comment</label>
           <!-- <input type="comment" placeholder="Write your comment" id="comment"/> -->
           <!--TEXTAREA FROM W 3 SCHOOL-->
-          <textarea id="comment" placeholder="Comment" name="pizza-comment" rows="4" cols="50" required ></textarea>
+          <textarea id="comment" placeholder="Comment" name="pizza-comment" rows="4" cols="50" required >
+<?php
+if (isset($_POST['pizza-comment'])){echo $_POST['pizza-comment'];}
+ ?>
+          </textarea>
 
           <!--TEXTAREA FROM W 3 SCHOOL-->
           <i class="fa fa-check-circle"></i>
@@ -142,21 +183,33 @@ body { margin: 0; padding: 0; }
 
             <label id="label-phone-number" for="user-phone">Write your phone number</label><br></br>
 
-              <input id="userPhone" type="tel" name:"phone" size="30" maxlength="10" placeholder="Phone number" value="" required />
+            <input type="tel" id="user-phone" name:"phone" size="30" maxlength="10" placeholder="Phone number" value="" required />
 
         </div>
 
         <div style="margin-bottom:1em">
           <p>Please select your favorite size:</p>
-             <input type="radio" id="radioFamily" name="size" value="family" style="margin-left:0.7em">
+             <input type="radio" id="radioFamily" name="size" value="family" style="margin-left:0.7em" <?php
+            if (isset($_POST['size']) && $_POST['size'] == 'family'){
+                echo 'checked="checked"';
+            }
+        ?>  >
 
              <label for="radioFamily">Family</label><br>
 
-             <input type="radio" id="radioLarge" name="size" value="large">
+             <input type="radio" id="radioLarge" name="size" value="large" <?php
+            if (isset($_POST['size']) && $_POST['size'] == 'large'){
+                echo 'checked="checked"';
+            }
+        ?> >
 
              <label for="radioLarge">Large</label><br>
 
-             <input type="radio" id="radioSmall" name="size" value="small">
+             <input type="radio" id="radioSmall" name="size" value="small" <?php
+            if (isset($_POST['size']) && $_POST['size'] == 'small'){
+                echo 'checked="checked"';
+            }
+        ?> >
 
              <label for="radioSmall">Small</label>
 
@@ -166,36 +219,146 @@ body { margin: 0; padding: 0; }
 
             <p id="pizzaTypes">Type</p>
 
-              <select name="type" size="4" multiple="multiple" id="pizzaType">
-                <option id="pizza1type" value="Pizza 1">Pizza 1</option>
-                <option id="pizza2type" value="Pizza 2">Pizza 2</option>
-                <option id="pizza3type" value="Pizza 3">Pizza 3</option>
-                <option id="pizza4type" value="Pizza 4">Pizza 4</option>
-                <option id="pizza5type" value="Pizza 5">Pizza 5</option>
-                <option id="pizza6type" value="Pizza 6">Pizza 6</option>
-              </select>
+              <?php
+              $pizzaTypesArr = array('Pizza 1', 'Pizza 2', 'Pizza 3','Pizza 4','Pizza 5','Pizza 6');
+              ?>
+
+              <select name="type" size="4" id="pizzaType">
+                      <?php
+                          foreach($pizzaTypesArr  as  $pizzaTypesEl){
+
+                              if ($_POST['type'] == $pizzaTypesEl) {
+                                  // afiseaza optiunea deja selectata
+                                  echo '<option selected="selected" value="' .$pizzaTypesEl. '">' . $pizzaTypesEl . '</option>';
+                              } else {
+                                  // default
+                                  echo '<option value="' .$pizzaTypesEl. '">' . $pizzaTypesEl . '</option>';
+                              }
+
+                          }
+                      ?>
+                  </select>
 
         </div>
+
 
 
         <div class="form-control">
 
             <p>Delivery Info</p>
 
-              <select name="DeliveryTime" id="deliveryTime">
-                <option value="6.30">6.30</option>
-                <option value="7.30">7.30</option>
-                <option value="8.30">8.30</option>
-              </select>
+
+            <?php
+            $deliveryHours = array("6.30", "7.30", "8.30");
+            ?>
+
+            <select name="deliveryTime" id="deliveryTime">
+                    <?php
+                        foreach($deliveryHours  as  $deliveryKeyIndex => $deliveryValueHour){
+
+                            if ($_POST['deliveryTime'] == $deliveryValueHour) {
+                                // afiseaza optiunea deja selectata
+                                echo '<option selected="selected" value="'. $deliveryValueHour . '">' . $deliveryValueHour . '</option>';
+                            } else {
+                                // default
+                                echo '<option value="' . $deliveryValueHour . '">' . $deliveryValueHour . '</option>';
+                            }
+
+                        }
+                    ?>
+                </select>
 
         </div>
 
 
+
+
+
+        <!-- <div class="form-control"> -->
+<p>Only if you want, you can choose some of the ingredients:</p>
+
+
+<label for="saltId">Salt:</label>
+<input type="checkbox" name="salt" id="saltId" <?php
+//CA SA RAMANA BIFAT DE CATRE USER
+if( isset($_POST['salt']) ){
+    echo 'checked="checked"' ;
+}
+?>  /><br>
+
+
+
+<label for="pepperId">Pepper:</label>
+<input type="checkbox" name="pepper" id="pepperId" <?php
+//CA SA RAMANA BIFAT DE CATRE USER
+if( isset($_POST['pepper']) ){
+    echo 'checked="checked"' ;
+}
+?> /><br>
+
+
+
+
+<label for="oliveId">Olive:</label>
+<input type="checkbox" name="olive"  id="oliveId"  <?php
+//CA SA RAMANA BIFAT DE CATRE USER
+if( isset($_POST['olive']) ){
+    echo 'checked="checked"' ;
+}
+?> />
+
+
+
+<label for="mushroomsId">Mushrooms:</label>
+<input type="checkbox" name="mushrooms" id="mushroomsId" <?php
+//CA SA RAMANA BIFAT DE CATRE USER
+if( isset($_POST['mushrooms']) ){
+    echo 'checked="checked"' ;
+}
+?> /><br>
+
+
+
+
+<label for="cheeseId">Cheese:</label>
+<input type="checkbox" name="cheese"  id="cheeseId"  <?php
+//CA SA RAMANA BIFAT DE CATRE USER
+if( isset($_POST['cheese']) ){
+    echo 'checked="checked"' ;
+}
+?> /><br>
+
+
+        <!-- </div> -->
+
+
+
+        <div style="padding:1.5em 0 1.5em 0">
+          <a href="termsAndConditions.html" style="border:1px solid DarkGoldenRod ;
+                                                   padding:0.3em ;
+                                                   border-radius:10px ;
+                                                   box-shadow: 1px 1px 1.5px 1.5px gray;
+                                                   text-align:center ;
+                                                   color:DarkGoldenRod">
+      Terms and conditions info
+          </a><br>
+        </div>
+
+
+
+          <label for="bifaId">Terms and conditions</label><br>
+          <input type="checkbox" id="bifaId"  name="bifa" <?php
+                      if (isset($_POST['bifa'])){
+                          echo 'checked="checked"';
+                      }
+                  ?>/><br/>
+
+
+
+
+
+
         <div class="form-control">
-
-              <!-- <input type="submit" value="Submit order" /> -->
-              <input id="resetPizza" type="reset" value="Reset order" required /><br>
-
           <input type="submit" name="submit" id="submit" value="Submit">
         </div>
 
@@ -212,26 +375,104 @@ body { margin: 0; padding: 0; }
   </div>
   <!------------------------------------------------------------------------------------------------>
 
-  <div class="container">
 
+  <div class="container"  style="background-color:floralwhite ; border-radius: 10px ; box-shadow: 1px 1px 1.5px 1.5px gray ; padding:1em 0 ; margin-top: 5em">
     <?php
 
 
+  echo '<h3 style="text-align:center ; color:DarkGoldenRod">Afisare date formular:</h3>';
 
-echo '<h3 style="text-align:center ; color:DarkGoldenRod ; margin-top:3em">Afisare date formular</h3>';
+  if($_POST['bifa'] && isset($_POST['submit'])){
 
-if(isset($_POST['submit'])){
+  echo '<h3 style="text-align:center ; color:DarkGoldenRod">Username is : ' . $_POST['name'] . '.</h3>' ;
+  echo '<h3 style="text-align:center ; color:DarkGoldenRod">Email address is : ' . $_POST['email'] . '.</h3>' ;
+  echo '<h3 style="text-align:center ; color:DarkGoldenRod">Your birthday is on  : ' . $_POST['dateBirthday'] . '.</h3>' ;
 
-echo '<h3 style="text-align:center ; color:DarkGoldenRod">Username is : ' . $_POST['name'] . '.</h3>' ;
-echo '<h3 style="text-align:center ; color:DarkGoldenRod">Email address is : ' . $_POST['email'] . '.</h3>' ;
-echo '<h3 style="text-align:center ; color:DarkGoldenRod">Password is : ' . $_POST['password'] . '.</h3>' ;
-echo '<h3 style="text-align:center ; color:DarkGoldenRod">User comment is : ' . $_POST['pizza-comment'] . '.</h3>' ;
-echo '<h3 style="text-align:center ; color:DarkGoldenRod">Phone number is : ' . $_POST['phone'] . '.</h3>' ;
 
-}
+  if($_POST['password'] === $_POST['password2']){
+  echo '<h3 style="text-align:center ; color:green">Password is : ' . $_POST['password'] . '.</h3>' ;
+  }else{
+  echo '<h3 style="text-align:center ; color:red">Passwords do not match.</h3>' ;
+  }
+
+  echo '<h3 style="text-align:center ; color:DarkGoldenRod">User comment is : ' . $_POST['pizza-comment'] . '.</h3>' ;
+
+  echo '<h3 style="text-align:center ; color:DarkGoldenRod">Phone number is : ' . $_POST['phone'] . '.</h3>' ;
+
+  echo '<h3 style="text-align:center ; color:DarkGoldenRod">Size of pizza  is : ' . $_POST['size'] . '.</h3>' ;
+
+  echo '<h3 style="text-align:center ; color:DarkGoldenRod">Type of pizza  is : ' . $_POST['type'] . '.</h3>' ;
+
+  echo '<h3 style="text-align:center ; color:DarkGoldenRod">Time of delivery  is : ' . $_POST['deliveryTime'] . '.</h3>' ;
+
+
+
+  if ( isset($_POST['salt'])){
+      //folosesc if  ca sa verific daca bifa de salt a fost selectata
+      //ca sa-mi afiseze pasiunea 'salt' in loc de 'on'
+      // echo '<h1 style="text-align:center ; color:DarkGoldenRod">The chosen ingredient is :' . $_POST['salt'] . '.</h1>' ;
+      echo '<h3 style="text-align:center ; color:green">The chosen ingredient is : salt .</h3>' ;
+  }else{
+      echo '<h3 style="text-align:center ; color:red">Salt is not a  chosen ingredient.</h3>' ;
+  }
+
+
+
+  if ( isset($_POST['pepper'])){
+      //folosesc if  ca sa verific daca bifa de pepper a fost selectata
+      //ca sa-mi afiseze pasiunea 'pepper' in loc de 'on'
+      // echo '<h1 style="text-align:center ; color:DarkGoldenRod">The chosen ingredient is :' . $_POST['pepper'] . '.</h1>' ;
+      echo '<h3 style="text-align:center ; color:green">The chosen ingredient is : pepper .</h3>' ;
+  }else{
+      echo '<h3 style="text-align:center ; color:red">Pepper is not a  chosen ingredient.</h3>' ;
+  }
+
+
+
+  if ( isset($_POST['olive'])){
+      //folosesc if  ca sa verific daca bifa de olive a fost selectata
+      //ca sa-mi afiseze pasiunea 'olive' in loc de 'on'
+      // echo '<h1 style="text-align:center ; color:DarkGoldenRod">The chosen ingredient is :' . $_POST['olive'] . '.</h1>' ;
+      echo '<h3 style="text-align:center ; color:green">The chosen ingredient is : olive .</h3>' ;
+  }else{
+      echo '<h3 style="text-align:center ; color:red">Olive is not a  chosen ingredient.</h3>' ;
+  }
+
+
+  if ( isset($_POST['mushrooms'])){
+      //folosesc if  ca sa verific daca bifa de mushrooms a fost selectata
+      //ca sa-mi afiseze pasiunea 'mushrooms' in loc de 'on'
+      // echo '<h1 style="text-align:center ; color:DarkGoldenRod">The chosen ingredient is :' . $_POST['mushrooms'] . '.</h1>' ;
+      echo '<h3 style="text-align:center ; color:green">The chosen ingredient is : mushrooms .</h3>' ;
+  }else{
+      echo '<h3 style="text-align:center ; color:red">Mushrooms is not a  chosen ingredient.</h3>' ;
+  }
+
+
+  if ( isset($_POST['cheese'])){
+      //folosesc if  ca sa verific daca bifa de cheese a fost selectata
+      //ca sa-mi afiseze pasiunea 'cheese' in loc de 'on'
+      // echo '<h1 style="text-align:center ; color:DarkGoldenRod">The chosen ingredient is :' . $_POST['cheese'] . '.</h1>' ;
+      echo '<h3 style="text-align:center ; color:green">The chosen ingredient is : cheese .</h3>' ;
+  }else{
+      echo '<h3 style="text-align:center ; color:red">Cheese is not a  chosen ingredient.</h3>' ;
+  }
+
+
+
+
+  echo '<h3 style="text-align:center ; color:green">Terms and conditions  are checked : ' . $_POST['bifa'] . '.</h3>' ;
+
+
+
+  }
 
       ?>
+  </div>
 
+
+
+  <div class="container">
     <!-- <div class="user-data"></div> -->
 
     <div id="center-svg-pizza">
@@ -269,7 +510,7 @@ echo '<h3 style="text-align:center ; color:DarkGoldenRod">Phone number is : ' . 
 
   <!-------------------------------------------------------------------------------------------------------------->
 
-  <footer>
+  <footer style="background-color:floralwhite ; margin-bottom:-2.5em">
     <div class="container">
 
       <div id="icons">
@@ -286,7 +527,7 @@ echo '<h3 style="text-align:center ; color:DarkGoldenRod">Phone number is : ' . 
 
       <!--sources links-->
       <a href="#" title="show and hide source links" class="btn_show_hide">
-        <h3>sources</h3>
+        <h3 style="padding-bottom:2.5em">sources</h3>
       </a>
       <div id="divSources" class="hidden">
         <h5><a href="https://www.youtube.com/watch?v=rsd4FNGTRBw" target="_blank">
@@ -309,7 +550,7 @@ echo '<h3 style="text-align:center ; color:DarkGoldenRod">Phone number is : ' . 
             https://www.youtube.com/watch?v=Cu0OwlJNXuQ
           </a></h5>
 
-        <h5><a href="https://formsubmit.co/live-demo" target="_blank">
+        <h5 style="padding-bottom:2.5em"><a href="https://formsubmit.co/live-demo" target="_blank">
             https://formsubmit.co/live-demo
           </a></h5>
       </div>
@@ -323,6 +564,8 @@ echo '<h3 style="text-align:center ; color:DarkGoldenRod">Phone number is : ' . 
 
 <!------activeaza pt verificare in consola si inactivare php------->
 <!-- <script src="js/contact-form.js" type="text/javascript"></script> -->
+
+<script src="js/myPHPform.js" type="text/javascript"></script>
 
 <script src="js/map.js" type="text/javascript"></script>
 <script src="js/text-image.js" type="text/javascript"></script>
---
+++ php-form-pizza/js/myPHPform.js
@@ -0,0 +1,161 @@
+console.log("!!! helloo from myPHPform.js");
+
+
+
+
+//JavaScript Client-side Form Validation
+//https://www.youtube.com/watch?v=rsd4FNGTRBw
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+
+const email = document.getElementById('email');
+
+const password = document.getElementById('password');
+
+const password2 = document.getElementById('password2');
+
+const comment = document.getElementById('comment');
+
+const phoneUser = document.getElementById('userPhone');
+
+const pizzaType = document.getElementById("pizzaType");
+
+const deliveryTime= document.getElementById("deliveryTime");
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function checkInputs() {
+
+	//get the values from the inputs
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	console.log("usernameValue is : " , usernameValue);
+	const emailValue = email.value.trim();
+	console.log("emailValue is : " , emailValue);
+	const passwordValue = password.value.trim();
+	console.log("passwordValue is : " , passwordValue);
+	const password2Value = password2.value.trim();
+	console.log("passwor2dValue is : " , password2Value);
+
+	const commentValue = comment.value.trim();
+	console.log("your comment value is : " , commentValue);
+
+///phone problem
+  const phoneUserValue = phoneUser.value.trim();
+  console.log("your phone number value is : " , phoneUserValue);
+  ///phone problem
+
+	const pizzaTypeValue = pizzaType.value;
+	console.log("///pizzaTypeValue is : " , pizzaTypeValue);
+
+	const deliveryTimeValue = deliveryTime.value;
+	console.log("///deliveryTimeValue is : " , deliveryTimeValue);
+
+
+  //////////////////////////////////
+
+
+  function setErrorFor(input, message) {
+
+    event.preventDefault();
+
+  	const formControl = input.parentElement;
+  	const small = formControl.querySelector('small');
+  	//add error class
+  	formControl.className = 'form-control error';
+  	//add error message inside small
+  	small.innerText = message;
+  }
+
+  function setSuccessFor(input) {
+
+  	const formControl = input.parentElement;
+  	//add sucess class
+  	formControl.className = 'form-control success';
+  }
+
+  function isEmail(email) {
+  	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  function setErrorForTextarea(textarea, message) {
+
+  	  event.preventDefault();
+
+  	const formControl = textarea.parentElement;
+  	const small = formControl.querySelector('.form-control small');
+  	formControl.className = 'form-control error';
+  	small.innerText = message;
+  }
+
+  function setSuccessForTextarea(textarea) {
+
+  	const formControl = textarea.parentElement;
+  	formControl.className = 'form-control success';
+  }
+
+
+
+
+  ////////////////////////////
+
+//show error //add error class
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		//add sucess class
+		setSuccessFor(username);
+	}
+
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+
+	//show error //add error class
+		if(commentValue === '') {
+			setErrorForTextarea(comment, 'Comment cannot be blank');
+		} else {
+			//add sucess class
+			setSuccessForTextarea(comment);
+		}
+
+
+///phone problem
+		if(phoneUserValue === '') {
+			setErrorFor(phoneUser, 'Phone number cannot be blank');
+		} else {
+			//add sucess class
+			setSuccessFor(phoneUser);
+		}
+///phone problem
+
+
+}
+
+
+	checkInputs();
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
